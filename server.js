ac9111ab92a033f1a1fbb116f6d2ae5c079e70c9
Aditya Shekhar <aditya.shekhar9@byjus.com>
2023-03-01 14:21:44 +0530

    Fixes in routes

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(
 app.use(cors({ origin }));
 app.use(cookieParser());
 app.get("/", (req, res) => res.send("Server is healthy."));
-app.use("/api/auth", require("./auth/route"));
+app.use("/api/auth", require("./auth/route.js"));
 app.get("/admin", adminAuth, (req, res) => res.send("Admin Route"));
 app.get("/basic", userAuth, (req, res) => res.send("User Route"));
 
