ee2ff5731e35767471df2fc1593bba9afb02b864
Aditya Shekhar <aditya.shekhar9@byjus.com>
2023-03-01 16:13:41 +0530

    Port and Mongo Endpoint changes

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
 const Mongoose = require("mongoose");
 const env = process.env;
 const localDB =
-  // env === "production"
-  //   ? `mongodb+srv://shekharaditya7:Indian%407@cluster1.8m2t2fg.mongodb.net/user_db?retryWrites=true&w=majority`
-  //   :
-  "mongodb://localhost:27017/user_db";
+  env === "production"
+    ? `mongodb+srv://shekharaditya7:Indian%407@cluster1.8m2t2fg.mongodb.net/user_db?retryWrites=true&w=majority`
+    : "mongodb://localhost:27017/user_db";
 
 const connectDB = async () => {
   await Mongoose.connect(localDB, {
