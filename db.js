8756d70f77abf594594aed075891d22d805367bf
Aditya Shekhar <aditya.shekhar9@byjus.com>
2023-03-01 15:40:40 +0530

    Fixes in DB connection

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const Mongoose = require("mongoose");
 const env = process.env;
 const localDB =
-  env === "production"
-    ? `mongodb+srv://shekharaditya7:Indian%407@cluster1.8m2t2fg.mongodb.net/user_db?retryWrites=true&w=majority`
-    : "mongodb://localhost:27017/user_db";
+  // env === "production"
+  //   ? `mongodb+srv://shekharaditya7:Indian%407@cluster1.8m2t2fg.mongodb.net/user_db?retryWrites=true&w=majority`
+  //   :
+  "mongodb://localhost:27017/user_db";
 
 const connectDB = async () => {
   await Mongoose.connect(localDB, {
